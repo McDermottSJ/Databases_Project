fad3d9058778bcbd3d52c5b6bd41cf21dcae7eeb
Sean McDermott <mcdermottsj@vcu.edu>
2018-04-19 11:21:12 -0400

    asd

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-import MySQLdb
+import PyMySQL
 
 cnx={'host': 'aa1t78pyyq2oz9v.c4pbzdfasekc.us-east-1.rds.amazonaws.com', 'username': 'admin', 'password': 'FlaskTask2018', 'db': 'ebdb'}
 
@@ -10,8 +10,14 @@ db = MySQLdb.connect(cnx['host'],cnx['username'],cnx['password'], cnx['db'])
 
 @app.route('/')
 def hello_world():
-	db.query("""SELECT * FROM test;""")
-	r = db.store_results()
-	print(r.fetch_row())
+	cursor = db.cursor()
+	sql  = """INSERT INTO test VALUES (4, 'asd')"""
+	try:
+		cursor.execute(sql)
+		db.commit()
+	except:
+		db.rollback()
+	db.close()
+
 	return render_template('main.html')
     
