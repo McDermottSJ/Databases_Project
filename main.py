a2f3fcc1217f6bbf0493074d0557a0490905dd46
cohenta2 <cohenta2@vcu.edu>
2018-04-22 14:45:23 -0400

    Added a detailed transaction table

--- main.py
+++ main.py
@@ -15,29 +15,46 @@ def home():
    
 @app.route('/sales', methods=['GET', 'POST'])
 def sales():
+    return render_template('sales.html')
+
+@app.route('/lookup', methods=['GET', 'POST'])
+def lookup():
     if request.method == 'POST':
         print(request.form)
         trans_num = request.form.get('transactionNumber')
+        if trans_num: 
+            results = {}
+            items = db.query('SELECT *, quantity * price as "sub_total" FROM (select item_name, quantity FROM contains WHERE transaction_number={}) as t1 NATURAL JOIN menu_items'.format(trans_num))
+            if not items.as_dict():
+                return render_template('sales.html', error=True)
+            results['items'] = items.as_dict()
+            name = db.query('SELECT employee_id, concat (first_name, " ", last_name) as server_name FROM employee NATURAL JOIN ticket WHERE transaction_number={}'.format(trans_num))
+            print(name.as_dict())
+            results['server_name'] = name.as_dict()[0]['server_name']
+            results['employee_id'] = name.as_dict()[0]['employee_id']
+            sub_total = db.query('SELECT SUM(sub_total) AS total FROM (SELECT *, quantity * price AS "sub_total" FROM (SELECT item_name, quantity FROM contains WHERE transaction_number={}) as t1 NATURAL JOIN menu_items) as t2'.format(trans_num))
+            results['sub_total'] = sub_total.as_dict()[0]['total']
+            tip = db.query('SELECT tip, transaction_datetime FROM ticket WHERE transaction_number={}'.format(trans_num))
+            results['tip'] = tip.as_dict()[0]['tip'] 
+            results['date'] = tip.as_dict()[0]['transaction_datetime']
+            results['transaction_number'] = trans_num
+            print(results)
+            return render_template('sales.html', ticket=results)
+        else:
+            return render_template('sales.html', error=True)
+ 
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        print(request.form)
         begin_dt = request.form.get('beforeDate')
         end_dt = request.form.get('endDate')
-        if trans_num:
-            results = transaction_num_search(trans_num)
-            return render_template('sales.html', data=results)
-        elif begin_dt and end_dt:
-            results = transaction_date_search(begin_dt, end_dt)
+        if begin_dt and end_dt:
+            data = db.query('SELECT * FROM ticket WHERE date(transaction_datetime) <= "{}" AND date(transaction_datetime) >= "{}"'.format(end_dt, begin_dt))
+            print(data)
+            results = data.as_dict()
             return render_template('sales.html', data=results) 
         else:
-            return render_template('error.html')
-    print("idk wtf happened")
-    return render_template('sales.html')
+            return render_template('sales.html', error=True)
 
-def transaction_num_search(trans_num):
-    data = db.query('SELECT * FROM ticket WHERE transaction_number={}'.format(trans_num))
-    print(data)
-    return data.as_dict()
-
-def transaction_date_search(begin_dt, end_dt):
-    data = db.query('SELECT * FROM ticket WHERE date(transaction_datetime) <= "{}" AND date(transaction_datetime) >= "{}"'.format(end_dt, begin_dt))
-    print(data)
-    return data.as_dict()
 
