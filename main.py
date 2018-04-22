04c73eb164792d6cdf04d483ea570bb558f72b63
cohenta2 <cohenta2@vcu.edu>
2018-04-22 15:26:24 -0400

    Cleaned code

--- main.py
+++ main.py
@@ -24,19 +24,15 @@ def lookup():
         trans_num = request.form.get('transactionNumber')
         if trans_num: 
             results = {}
-            items = db.query('SELECT *, quantity * price as "sub_total" FROM (select item_name, quantity FROM contains WHERE transaction_number={}) as t1 NATURAL JOIN menu_items'.format(trans_num))
-            if not items.as_dict():
+            results['items'] = get_items(trans_num)
+            if not results['items']:
                 return render_template('sales.html', error=True)
-            results['items'] = items.as_dict()
-            name = db.query('SELECT employee_id, concat (first_name, " ", last_name) as server_name FROM employee NATURAL JOIN ticket WHERE transaction_number={}'.format(trans_num))
-            print(name.as_dict())
-            results['server_name'] = name.as_dict()[0]['server_name']
-            results['employee_id'] = name.as_dict()[0]['employee_id']
-            sub_total = db.query('SELECT SUM(sub_total) AS total FROM (SELECT *, quantity * price AS "sub_total" FROM (SELECT item_name, quantity FROM contains WHERE transaction_number={}) as t1 NATURAL JOIN menu_items) as t2'.format(trans_num))
-            results['sub_total'] = sub_total.as_dict()[0]['total']
-            tip = db.query('SELECT tip, transaction_datetime FROM ticket WHERE transaction_number={}'.format(trans_num))
-            results['tip'] = tip.as_dict()[0]['tip'] 
-            results['date'] = tip.as_dict()[0]['transaction_datetime']
+            server = get_server(trans_num)
+            results['server_name'] = server['server_name']
+            results['employee_id'] = server['employee_id']
+            results['sub_total'] = get_sub_total(trans_num) 
+            results['tip'] = get_tip(trans_num) 
+            results['date'] =  get_date(trans_num)
             results['transaction_number'] = trans_num
             print(results)
             return render_template('sales.html', ticket=results)
@@ -56,5 +52,26 @@ def search():
             return render_template('sales.html', data=results) 
         else:
             return render_template('sales.html', error=True)
+
+def get_items(trans_num):
+    items = db.query('SELECT *, quantity * price as "sub_total" FROM (select item_name, quantity FROM contains WHERE transaction_number={}) as t1 NATURAL JOIN menu_items'.format(trans_num))
+    return items.as_dict()
+
+
+def get_server(trans_num):
+    name = db.query('SELECT employee_id, concat (first_name, " ", last_name) as server_name FROM employee NATURAL JOIN ticket WHERE transaction_number={}'.format(trans_num))
+    return name.as_dict()[0]
+
+def get_sub_total(trans_num):
+    sub_total = db.query('SELECT SUM(sub_total) AS total FROM (SELECT *, quantity * price AS "sub_total" FROM (SELECT item_name, quantity FROM contains WHERE transaction_number={}) as t1 NATURAL JOIN menu_items) as t2'.format(trans_num))
+    return sub_total.as_dict()[0]['total']
+
+def get_tip(trans_num):
+    tip = db.query('SELECT tip FROM ticket WHERE transaction_number={}'.format(trans_num))
+    return tip.as_dict()[0]['tip']
+
+def get_date(trans_num):
+    date = db.query('SELECT transaction_datetime FROM ticket WHERE transaction_number={}'.format(trans_num))
+    return date.as_dict()[0]['transaction_datetime']
 
 
